31e9feb24b7cd6bcce283ed29cdd80ad0ce459e8
Jerrynanor <2425402814@live.gctu.edu.gh>
2026-05-31 06:35:56 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+# Name: Jerry Nanor
+# STUDENT ID: 2425402814
+# Email: 2425402814@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Jerry Nanor, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints the initials JNN using only basic print statements.
+    print()
+    print("...JNN")
+    print()
+    print("......JJJJJJJJJJ......NNNN....NNNN......NNNN....NNNN............**..")
+    print("......JJJJJJJJJJ......NNNNN...NNNN......NNNNN...NNNN............**..")
+    print("............JJJJ......NNNNNN..NNNN......NNNNNN..NNNN..........****..")
+    print("............JJJJ......NNNN.NN.NNNN......NNNN.NN.NNNN..........****..")
+    print("............JJJJ......NNNN..NNNNNN......NNNN..NNNNNN............**..")
+    print("............JJJJ......NNNN...NNNNN......NNNN...NNNNN............**..")
+    print("......JJ....JJJJ......NNNN....NNNN......NNNN....NNNN................")
+    print("......JJJJJJJJJJ......NNNN....NNNN......NNNN....NNNN................")
+    print("......JJJJJJJJJJ......NNNN....NNNN......NNNN....NNNN................")
+    print("........JJJJJJ........NNNN....NNNN......NNNN....NNNN................")
+    print()
 
 
 main()
